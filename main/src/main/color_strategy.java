551a23ec7c4fe28daa56ab3abff4ec2ea559a04e
jordannstrong <jordannstrong@gmail.com>
2016-02-17 19:21:21 -0500

    Interface cleanup

--- main/src/main/color_strategy.java
+++ main/src/main/color_strategy.java
@@ -7,7 +7,6 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-
 public class color_strategy {
 
     private static final String IMG_PATH = "main\\src\\main\\resources\\map.jpg";
@@ -38,29 +37,38 @@ public class color_strategy {
         ImageIcon icon = new ImageIcon(scaledMap);
         JLabel image = new JLabel(icon);
 
-
-
         //Panel with controls
-        JPanel controls = new JPanel();
-        JComboBox constraintSelection = new JComboBox(new String[] {"Location"});
+        JPanel controls1 = new JPanel();
         JComboBox constraintSelection2 = new JComboBox(new String[] {"Departure", "Arrival", "Overflight"});
         JComboBox constraintSelection3 = new JComboBox(new String[] {"PHL", "LAX", "ORD", "ATL", "JFK"});
-        controls.add(constraintSelection);
-        controls.add(constraintSelection2);
-        controls.add(constraintSelection3);
+        controls1.add(constraintSelection2);
+        controls1.add(constraintSelection3);
+        controls1.add(redColor);
 
+        JPanel controls2 = new JPanel();
+        JComboBox constraintSelection5 = new JComboBox(new String[] {"Departure", "Arrival", "Overflight"});
+        JComboBox constraintSelection6 = new JComboBox(new String[] {"PHL", "LAX", "ORD", "ATL", "JFK"});
+        controls2.add(constraintSelection5);
+        controls2.add(constraintSelection6);
+        controls2.add(greenColor);
 
+        JPanel buttons = new JPanel();
+        JButton loadDatabase = new JButton("Load database");
+        JButton generateVisualization = new JButton("Generate Visualization");
+        JButton reset = new JButton("Reset");
+
+        buttons.add(loadDatabase);
+        buttons.add(generateVisualization);
+        buttons.add(reset);
 
         //Add all items to respective panels
         frame.add(outer);
         outer.add(image);
         image.add(mapPanel);
         outer.add(side);
-        side.add(controls);
-        side.add(colors);
-        colors.add(redColor);
-        colors.add(greenColor);
-
+        side.add(controls1);
+        side.add(controls2);
+        side.add(buttons);
 
         frame.pack();
         frame.setVisible(true);
